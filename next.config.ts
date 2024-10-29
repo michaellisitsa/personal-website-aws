c65dcad257fd03f7291ae038549690f9a7d0ee84
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-10-29 21:43:33 +1100

    Add boilerplate nextjs project

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+
+  /* Allow static hosting */
+  output: "export",
+};
+
+export default nextConfig;
