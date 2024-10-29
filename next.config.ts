4d65cd28a3692b3aa8290abd86aba3d97a79dd84
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-10-30 08:32:35 +1100

    Image Optimization using the default loader is not compatible with export.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
 
   /* Allow static hosting */
   output: "export",
+
+  /* Image Optimization using the default loader is not compatible with export. */
+  images: {
+    unoptimized: true, // Disables Image Optimization for static exports
+  },
 };
 
 export default nextConfig;
+``;
