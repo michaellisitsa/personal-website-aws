15084b13bee41207546af7566f706873512f93a5
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-11-27 22:03:42 +1100

    Add custom diagram creator and some css fixes

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,5 +1,6 @@
 import ProjectItem from "./ProjectItem";
 import styles from "./ProjectsList.module.css";
+import customDiagramScreenshot from "@/assets/custom-diagram-screenshot.png";
 import serverlessPrintScreenshot from "@/assets/serverless-print-screenshot.png";
 import personalWebsiteScreenshot from "@/assets/personal-website-screenshot.png";
 import algoChallengesScreenshot from "@/assets/algo-challenges-screenshot.png";
@@ -9,22 +10,30 @@ export default function ProjectsList() {
     <div className={styles.root}>
       <h1>Projects List</h1>
       <ProjectItem
+        title="Custom Diagram Creator"
+        description="Custom Diagrams is a powerful method of plugging in rich
+         visualisations into ClearCalcs calculators"
+        githubLink="https://github.com/ClearCalcs/custom-diagram-boilerplate"
+        primaryLink="https://clearcalcs.github.io/custom-diagram-boilerplate"
+        heroImage={customDiagramScreenshot}
+      />
+      <ProjectItem
         title="Serverless Print"
-        description="A serverless print job submission system"
+        description="A serverless print job submission system using AWS API Gateway web sockets, Lambda, S3, and DynamoDB"
         primaryLink="/projects/print"
         githubLink="https://github.com/michaellisitsa/serverless-print-job"
         heroImage={serverlessPrintScreenshot}
       />
       <ProjectItem
         title="Personal Website"
-        description="The website you're on now!"
+        description="The website you're on now, hosted on AWS S3 and Cloudfront bootstrapped using Cloudfront Hosting Toolkit"
         githubLink="https://github.com/michaellisitsa/personal-website-aws"
         primaryLink="/"
         heroImage={personalWebsiteScreenshot}
       />
       <ProjectItem
         title="Algorithm Challenges"
-        description="A collection of coding challenges from Advent of Code and HackerRank"
+        description="A collection of coding challenges from Advent of Code and HackerRank in Javascript, Ruby, Python, C++ and more"
         githubLink="https://github.com/michaellisitsa/algo_challenges"
         primaryLink="https://github.com/michaellisitsa/algo_challenges"
         heroImage={algoChallengesScreenshot}
