1e1103e670da5a9619b692d7287323b0f1267ed0
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-12-15 21:05:49 +1100

    Add list of blogs I'm reading

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -2,9 +2,21 @@ import Link from "next/link";
 import styles from "./BlogList.module.css";
 export default function BlogList() {
   return (
-    <div className={styles.root}>
+    <main className={styles.root}>
       <h1>Blogs</h1>
-      <Link href="/blog/germany">Exchange Semester to Nuremberg 2011</Link>
-    </div>
+      <section>
+        <Link href="/blog/germany">Exchange Semester to Nuremberg 2011</Link>
+      </section>
+      <section>
+        Other Interesting Blogs I'm reading:
+        <ul>
+          <li>
+            <a href="https://indepth.dev/">
+              <span>In Depth Dev</span>
+            </a>
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 }
