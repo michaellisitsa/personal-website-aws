2ea92c0b24041e0edfe17e08dd4bac9ee3881f6b
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-03-24 20:47:22 +1100

    Add pygame game and Django projects

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,14 +1,23 @@
 import ProjectItem from "./ProjectItem";
 import styles from "./ProjectsList.module.css";
 import customDiagramScreenshot from "@/assets/custom-diagram-screenshot.png";
+import flexiblePlatformerGif from "@/assets/flexible-platformer-game.gif";
 import serverlessPrintScreenshot from "@/assets/serverless-print-screenshot.png";
 import personalWebsiteScreenshot from "@/assets/personal-website-screenshot.png";
 import algoChallengesScreenshot from "@/assets/algo-challenges-screenshot.png";
+import expenseTrackerScreenshot from "@/assets/expense-tracker-screenshot.jpg";
 
 export default function ProjectsList() {
   return (
     <div className={styles.root}>
       <h1>Projects List</h1>
+      <ProjectItem
+        title="Pygame Platform Side Scroller"
+        description="Platform Side Scroller where all the platforms are bendy. Using Pygame"
+        githubLink="https://github.com/michaellisitsa/flexible-beam-platformer"
+        primaryLink="https://github.com/michaellisitsa/flexible-beam-platformer"
+        heroImage={flexiblePlatformerGif}
+      />
       <ProjectItem
         title="Custom Diagram Creator"
         description="Custom Diagrams is a powerful method of plugging in rich
@@ -37,6 +46,13 @@ export default function ProjectsList() {
         githubLink="https://github.com/michaellisitsa/algo_challenges"
         primaryLink="https://github.com/michaellisitsa/algo_challenges"
         heroImage={algoChallengesScreenshot}
+      />
+      <ProjectItem
+        title="Django and React Expense Tracker"
+        description="A full Django backend and React Frontend web app to manage personal expenses"
+        githubLink="https://github.com/michaellisitsa/expense_tracker"
+        primaryLink="https://github.com/michaellisitsa/expense_tracker"
+        heroImage={expenseTrackerScreenshot}
       />
     </div>
   );
